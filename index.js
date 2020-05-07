e90542de8b55d8d58c14724ec24b2eada9c80132
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-07 21:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 // Write your code here
+function multiply() {
+  let num1 = 31;
+  let num2 = 2;
+  let multiply = num1 * num2;
+  return multiply;
+}
 let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
