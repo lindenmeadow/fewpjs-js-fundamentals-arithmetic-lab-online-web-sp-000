ba4b825d66592caa29cb70078d2c1e0dddf41649
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-07 21:34:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+  let num1 = 31
+  let num2 = 2
+  const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = 24 % 5
+
+const max = Math.max(12, 20, 3, 5)
