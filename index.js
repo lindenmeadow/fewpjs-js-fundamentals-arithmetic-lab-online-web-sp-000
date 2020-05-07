0906bfc3e3aa21874538afd5055b00d21c2ce9a4
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-07 21:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 // Write your code here
-  let num1 = 31;
-  let num2 = 2;
-  const multiply = num1 * num2;
+  let num1 = 31
+  let num2 = 2
+  const multiply = num1 * num2
 
-const random = return Math.floor(Math.random() * 10) + 1;
+const random = return Math.floor(Math.random() * 10) + 1
 
-const mod = 24 % 5;
+const mod = 24 % 5
 
-const max = Math.max(12, 20, 3, 5);
+const max = Math.max(12, 20, 3, 5)
